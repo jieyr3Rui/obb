d95e9bd5de2bc76f16931e7c4eb00cb3e2cb4956
jieyr3Rui <2930681195@qq.com>
2019-12-01 11:21:48 +0800

    commit

--- utils/is_point_in.py
+++ utils/is_point_in.py
@@ -1,28 +1,20 @@
 import numpy as np
  
-# 判断该点是否在该条线上
- 
-# point:点坐标,如:[113.775698, 30.236892]
-# o : 该条线的起点
-# d : 该条线的终点
 def is_point_in_line(point, o, d, command='2d'):
     if command == '2d':
-        # 先判断该点是否在线段范围内,如果不在,
-        # 则就算在该方程的直线上但也不在该线段上
-        if o[1] > point[1] and d[1] > point[1]:  # 该点纵坐标小于线段最小纵坐标
+        if o[1] > point[1] and d[1] > point[1]:
             return False
-        if o[1] < point[1] and d[1] < point[1]:  # 该点纵坐标大于线段最大纵坐标
+        if o[1] < point[1] and d[1] < point[1]:
             return False
-        if o[0] > point[0] and d[0] > point[0]:  # 该点横坐标小于线段最小横坐标
+        if o[0] > point[0] and d[0] > point[0]:
             return False
-        if o[0] < point[0] and d[0] < point[0]:  # 该点横坐标大于线段最大横坐标
+        if o[0] < point[0] and d[0] < point[0]:
             return False
     
-        # 若线段为垂直直线,则该点的横坐标等于该线段的横坐标才说明在该线段上
         if o[0] == d[0]:
             return True if point[0] == o[0] else False
     
-        # 通过输入的两个点计算一元一次方程,通过输入x,计算y
+
         a = (d[1] - o[1]) / (d[0] - o[0])
         b = (d[0] * o[1] - o[0] * d[1]) / (d[0] - o[0])
         y = a * point[0] + b
@@ -31,58 +23,35 @@ def is_point_in_line(point, o, d, command='2d'):
         return True if ((point > o) & point < d) else False
     return False
  
-# 假设以该点向右水平做射线,判断该点是否与该线段有交点(只要线段与射线相交则为true)
-# point:点坐标,如:[113.775698, 30.236892]
-# o : 该条线的起点
-# d : 该条线的终点
 def is_ray_intersects_segment(point, o, d):
-    if o[1] == d[1]:  # 如果线段是水平直线,则直接无交点
+    if o[1] == d[1]:
         return False
-    # 先判断该点是否在线段范围内,如果不在,
-    # 则就算在该方程的直线上但也不在该线段上
-    if o[1] > point[1] and d[1] > point[1]:  # 该点纵坐标小于等于线段最小纵坐标
+    if o[1] > point[1] and d[1] > point[1]:
         return False
-    if o[1] < point[1] and d[1] < point[1]:  # 该点纵坐标大于等于线段最大纵坐标
+    if o[1] < point[1] and d[1] < point[1]:
         return False
  
-    # 先求出一元一次方程求交点
-    # 若线段为垂直直线,则该点的横坐标应该小于该点的横坐标才能有交点
-    # if o[0] == d[0]:
-    #     return True if point[0] < o[0] else False
-    #
-    # # 通过输入的两个点计算一元一次方程,通过输入x,计算y
-    # a = (d[1] - o[1]) / (d[0] - o[0])
-    # b = (d[0] * o[1] - o[0] * d[1]) / (d[0] - o[0])
-    # x = (point[1] - b) / a
- 
-    # 利用三角形比例关系求交点
     x = d[0] - (d[0] - o[0]) * (d[1] - point[1]) / (d[1] - o[1])
  
-    # 如果该点的横坐标小于相同水平线上交点的横坐标,则有交点
     return True if point[0] < x else False
  
 
 def is_point_in_polygon(point_coord, polygon_coords, is_contains_edge):
-    intersect_count = 0  # 交点个数
+    intersect_count = 0 
     for polygon in polygon_coords:
         polygon = polygon.T
-        # 循环每条边
         for i in range(len(polygon) - 1):
             origin_point = polygon[i]
             destination_point = polygon[i + 1]
  
-            # 是否包含存在直线上的点
             if is_point_in_line(point_coord, origin_point, destination_point):
                 return True if is_contains_edge else False
  
             if is_ray_intersects_segment(point_coord, origin_point, destination_point):
-                # 有交点就加1
                 intersect_count += 1
  
-    # 如果恰好与端点相交,则查看相应的端点,并减去与该点相同纵坐标且在该点右侧的点的个数
     endpoint_intersects_count = 0
     for polygon in polygon_coords:
-        # 遍历每一个点,但是因为最后一个点和第一个点是重复的,所以最后一个不遍历
         for i in polygon[:-1]:
             if i[1] == point_coord[1] and i[0] > point_coord[0]:
                 endpoint_intersects_count += 1
